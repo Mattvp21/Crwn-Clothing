9ad7e032250c45ee07d194be2476a6b8e4a13a05
Matthew Van Pelt <74370212+Mattvp21@users.noreply.github.com>
2021-06-14 09:54:59 -0400

    Updating our app with a shop page/collection prev

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Switch,Router,Route, withRouter} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/homepage/shop.component.jsx';
+
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/shop' component={ShopPage} />
+    
+    </Switch>
     </div>
   );
 }
