eca34e869f19df91085cd5e20c4d701243d2b1b5
Matthew Van Pelt <74370212+Mattvp21@users.noreply.github.com>
2021-07-28 09:50:44 -0400

    firebase google auth added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Switch,Router,Route, withRouter} from 'react-router-dom';
+import {Switch, Route} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/homepage/shop.component.jsx';
+import Header from './components/header/header-component.jsx';
+import signInAndSignUpPage from './pages/homepage/signin-signup/signin-signup.component'
 
 
 
 function App() {
   return (
     <div>
+    <Header />
     <Switch>
       <Route exact path='/' component={HomePage} />
       <Route path='/shop' component={ShopPage} />
-    
+      <Route path='/signin' component={signInAndSignUpPage} />
     </Switch>
     </div>
   );
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyC2dT1T9GcDsxp5ju_oFOeIko_my7QEV_o",
+    authDomain: "crwn-clothing-b63e7.firebaseapp.com",
+    projectId: "crwn-clothing-b63e7",
+    storageBucket: "crwn-clothing-b63e7.appspot.com",
+    messagingSenderId: "307080648303",
+    appId: "1:307080648303:web:27862820cf421ad11a5f9a",
+    measurementId: "G-8P0WG05K77"
+  };
+
+  firebase.initializeApp(config);
+
+  export const auth = firebase.auth();
+  export const firestore = firebase.firestore();
+  const provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({ prompt: 'select_account'})
+  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+  export default { signInWithGoogle };
