95b27e528816679a9883877c565edb9f54b5da5b
Matthew Van Pelt <74370212+Mattvp21@users.noreply.github.com>
2021-07-30 16:34:15 -0400

    Implemented firebase utilities

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/homepage/shop.component.jsx';
 import Header from './components/header/header-component.jsx';
 import signInAndSignUpPage from './pages/homepage/signin-signup/signin-signup.component'
-import { auth } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 
 class App extends React.Component {
@@ -21,10 +21,22 @@ class App extends React.Component {
   unsubcribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+     if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data()
+              }
+          })
+          console.log(this.state)
+        });
+       
+     }
 
-      console.log(user);
+     this.setState({currentUser:userAuth});
   });
   }
 
--- src/pages/homepage/signin-signup/signin-signup.component.jsx
+++ src/pages/homepage/signin-signup/signin-signup.component.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 import './signin-signup.styles.scss'
 
 import SignIn from '../../../components/sign-in/sign-in.component'
-
+import Signup from '../../../components/sign-up/sign-up.component';
 const signInAndSignUpPage = () => (
     <div className='sign-in-and-sign-up'>
     <SignIn />
+    <Signup />
     </div>
 );
 
